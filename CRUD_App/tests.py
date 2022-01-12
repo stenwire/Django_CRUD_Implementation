886275b9c08c001db07b5f30794a5b9687f5c4ec
Stephen Nwankwo <nwankwostephen039@yahoo.com>
2022-01-12 02:04:53 +0100

    feat added a new feature

--- CRUD_App/tests.py
+++ CRUD_App/tests.py
@@ -1,11 +1,11 @@
-from django.test import TestCase
 from django.contrib.auth import get_user_model
+from django.test import TestCase
 from django.urls import reverse
 from .models import Post
 
 
 # Create your tests here.
-class BlogTest(TestCase):
+class BlogTests(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(
             username='testuser',
@@ -16,12 +16,20 @@ class BlogTest(TestCase):
         self.post = Post.objects.create(
             title='A good title',
             body='Nice body',
-            author=self.user,
+            author=self.user
         )
 
+
+    def test_user_exists(self):
+        user_count = User.objects.all().count()
+        print(user_count)
+        self.assertEqual(user_count, 1
+        self.assertEqual(user_count, 0)
+
+        """
     def test_string_representative(self):
-        post = Post(title='A simple title')
-        self.assertEqual(str(post), post.title)
+        post = Post(title='A good title')
+        self.assertEqual(str(Post), post.title)
 
     def test_post_content(self):
         self.assertEqual(f'{self.post.title}', 'A good title')
@@ -30,4 +38,15 @@ class BlogTest(TestCase):
 
     def test_post_list_view(self):
         response = self.client.get(reverse('home'))
-        self.assertEqual()
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nice body')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_post_detail_views(self):
+        response = self.client.get('/post/1/')
+        no_response = self.client.get('/post/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'A good title')
+        self.assertTemplateUsed(response, 'post_detail.html')
+"""
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Django_CRUD_Implementation
-A django blog project illutrating the application of django CRUD.
+A django blog project illustrating the application of django CRUD.
+
+# Steps to run this project
+####If you would like to run this project on your end, plese follow the steps below
+>1. clone the repo
+>>>>>>git clone @repo
+>
+>2.Activate virtual environmets and install requirements from requirement.text file
+>>>>>>pip install 'requiremets.txt'
